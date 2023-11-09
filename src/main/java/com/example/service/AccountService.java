8f9cce3ebc2a2cf214e54177d648b6568a457554
Tom Moran <tmora76@wgu.edu>
2023-11-09 20:48:17 +0000

    1st commit

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,26 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+    AccountRepository accountRepository;
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+    public Account persistAccount(Account account){
+        return accountRepository.save(account);
+    }
+    
+    
+
+    
+
+    
+    
 }
